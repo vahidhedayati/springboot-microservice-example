a6fb0f54d61e9fcdb2ff7dfc3679a684fa4c31a0
V Hedayati <badvad@gmail.com>
2022-05-12 20:07:40 +0100

    tidyup better information on usage

--- gateway/src/main/java/com/vh/test/microservice/controller/MainController.java
+++ gateway/src/main/java/com/vh/test/microservice/controller/MainController.java
@@ -47,13 +47,45 @@ public class MainController {
      */
 
 
+    /**
+     * Below are 3 REST calls using POST / GET / DELETE methods for the ip:port/{methodName} followed by
+     * what ever the function requires as description below
+     *
+     */
 
+
+    /**
+     * POST as per above method names to i.e.
+     * http://localhost:8085/arrayList/
+     * POST BODY: { "elements" : "1000", "text": "what array elements text should be"  }
+     *
+     * http://localhost:8085/arrayBlockingList/
+     * POST BODY:  { "elements" : "1000", "text": "what array elements text should be"  }
+     *
+     * The body is searchBean object as json
+     *
+     *
+     * Using searchBean content:
+     * @param methodName
+     * @param content
+     * @return
+     */
     @PostMapping("/{methodName}")
     public ResponseEntity<Flux<Map<String, Object>>>  list(@PathVariable String methodName, @RequestBody SearchBean content){
         Map<String,Object> response = restTemplate.postForObject("http://ARRAY-LIST/"+methodName+"/",generateHttpEntity(content.getEntries(),content.getText()), Map.class);
         return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
     }
 
+    /**
+     * GET as per above method names to i.e.
+     * (only if you have first used above to fill the contents of the given method)
+     * http://localhost:8085/arrayList/1
+     *
+     *  http://localhost:8085/concurrentHashMap/1
+     * @param methodName
+     * @param id
+     * @return
+     */
     @GetMapping("/{methodName}/{id}")
     public ResponseEntity<Flux<Map<String, Object>>>  findListByIndex(@PathVariable String methodName, @PathVariable Integer id){
         Map<String,Object> response = restTemplate.getForObject("http://ARRAY-LIST/"+methodName+"/"+id, Map.class);
@@ -61,143 +93,21 @@ public class MainController {
     }
 
 
+    /**
+     *  DELETE as per above method names to i.e.
+     *  (only if you have first used above to fill the contents of the given method)
+     *  http://localhost:8085/arrayList/1
+     *
+     *  http://localhost:8085/concurrentHashMap/1
+     *
+     * @param methodName
+     * @param id
+     * @return
+     */
     @DeleteMapping("/{methodName}/{id}")
     public ResponseEntity<Flux<Map>> removeFromListByIndex(@PathVariable String methodName, @PathVariable Integer id) {
         ResponseEntity<Map> response =  restTemplate.exchange("http://ARRAY-LIST/arrayList/"+methodName+"/"+id, HttpMethod.DELETE,null, Map.class);
         return new ResponseEntity(Flux.just(response), response.getStatusCode());
     }
-
-    /**
-     * All of below methods covered by the 3 above methods which parse methodName (only difference between them all)
-     *
-
-
-    @PostMapping("/arrayList")
-    public ResponseEntity<Flux<Map<String, Object>>>  arrayList(@RequestBody SearchBean content){
-        Map<String,Object> response = restTemplate.postForObject("http://ARRAY-LIST/arrayList/",generateHttpEntity(content.getEntries(),content.getText()), Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-    @GetMapping("/arrayList/{id}")
-    public ResponseEntity<Flux<Map<String, Object>>>  findArrayListByIndex(@PathVariable Integer id){
-        Map<String,Object> response = restTemplate.getForObject("http://ARRAY-LIST/arrayList/"+id, Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-
-    @DeleteMapping("/arrayList/{id}")
-    public ResponseEntity<Flux<Map>> removeFromArrayListByIndex(@PathVariable Integer id) {
-        ResponseEntity<Map> response =  restTemplate.exchange("http://ARRAY-LIST/arrayList/"+id, HttpMethod.DELETE,null, Map.class);
-        return new ResponseEntity(Flux.just(response), response.getStatusCode());
-    }
-
-
-    @PostMapping("/linkList")
-    public ResponseEntity<Flux<Map<String, Object>>>  fillLinkListArrayList(@RequestBody SearchBean content){
-        Map<String,Object> response = restTemplate.postForObject("http://ARRAY-LIST/linkList/",generateHttpEntity(content.getEntries(),content.getText()), Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-    @GetMapping("/linkList/{id}")
-    public ResponseEntity<Flux<Map<String, Object>>>  findLinkedListByIndex(@PathVariable Integer id){
-        Map<String,Object> response = restTemplate.getForObject("http://ARRAY-LIST/linkList/"+id, Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-
-    @DeleteMapping("/linkList/{id}")
-    public ResponseEntity<Flux<Map>> removeFromLinkedListByIndex(@PathVariable Integer id) {
-        ResponseEntity<Map> response =  restTemplate.exchange("http://ARRAY-LIST/linkList/"+id, HttpMethod.DELETE,null, Map.class);
-        return new ResponseEntity(Flux.just(response), response.getStatusCode());
-    }
-
-
-
-    @PostMapping("/stringArray")
-    public ResponseEntity<Flux<Map<String, Object>>>  fillStringArrayList(@RequestBody SearchBean content){
-        Map<String,Object> response = restTemplate.postForObject("http://ARRAY-LIST/stringArray/",generateHttpEntity(content.getEntries(),content.getText()), Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-    @GetMapping("/stringArray/{id}")
-    public ResponseEntity<Flux<Map<String, Object>>>  findStringArrayListByIndex(@PathVariable Integer id){
-        Map<String,Object> response = restTemplate.getForObject("http://ARRAY-LIST/stringArray/"+id, Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-
-    @DeleteMapping("/stringArray/{id}")
-    public ResponseEntity<Flux<Map>> removeFromStringArrayListByIndex(@PathVariable Integer id) {
-        ResponseEntity<Map> response =  restTemplate.exchange("http://ARRAY-LIST/stringArray/"+id, HttpMethod.DELETE,null, Map.class);
-        return new ResponseEntity(Flux.just(response), response.getStatusCode());
-    }
-
-
-
-    @PostMapping("/arrayBlockingList")
-    public ResponseEntity<Flux<Map<String, Object>>>  findArrayBlockingListByIndex(@RequestBody SearchBean content){
-        Map<String,Object> response = restTemplate.postForObject("http://ARRAY-LIST/arrayBlockingList/",generateHttpEntity(content.getEntries(),content.getText()), Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-    @GetMapping("/arrayBlockingList/{id}")
-    public ResponseEntity<Flux<Map<String, Object>>>  findArrayBlockingListByIndex(@PathVariable Integer id){
-        Map<String,Object> response = restTemplate.getForObject("http://ARRAY-LIST/arrayBlockingList/"+id, Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-
-    @DeleteMapping("/arrayBlockingList/{id}")
-    public ResponseEntity<Flux<Map>> removeFromArrayBlockingListByIndex(@PathVariable Integer id) {
-        ResponseEntity<Map> response =  restTemplate.exchange("http://ARRAY-LIST/arrayBlockingList/"+id, HttpMethod.DELETE,null, Map.class);
-        return new ResponseEntity(Flux.just(response), response.getStatusCode());
-    }
-
-
-
-    @PostMapping("/treeSet")
-    public ResponseEntity<Flux<Map<String, Object>>>  fillTreeSet(@RequestBody SearchBean content){
-        Map<String,Object> response = restTemplate.postForObject("http://ARRAY-LIST/treeSet/",generateHttpEntity(content.getEntries(),content.getText()), Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-    @GetMapping("/treeSet/{id}")
-    public ResponseEntity<Flux<Map<String, Object>>>  findTreeSetByIndex(@PathVariable Integer id){
-        Map<String,Object> response = restTemplate.getForObject("http://ARRAY-LIST/treeSet/"+id, Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-
-    @DeleteMapping("/treeSet/{id}")
-    public ResponseEntity<Flux<Map>> removeFromTreeSetByIndex(@PathVariable Integer id) {
-        ResponseEntity<Map> response =  restTemplate.exchange("http://ARRAY-LIST/treeSet/"+id, HttpMethod.DELETE,null, Map.class);
-        return new ResponseEntity(Flux.just(response), response.getStatusCode());
-    }
-
-
-    @PostMapping("/stack")
-    public ResponseEntity<Flux<Map<String, Object>>>  fillStack(@RequestBody SearchBean content){
-        Map<String,Object> response = restTemplate.postForObject("http://ARRAY-LIST/stack/",generateHttpEntity(content.getEntries(),content.getText()), Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-    @GetMapping("/stack/{id}")
-    public ResponseEntity<Flux<Map<String, Object>>>  findStackByIndex(@PathVariable Integer id){
-        Map<String,Object> response = restTemplate.getForObject("http://ARRAY-LIST/stack/"+id, Map.class);
-        return new ResponseEntity<Flux<Map<String, Object>>>(Flux.just(response), HttpStatus.OK);
-    }
-
-
-    @DeleteMapping("/stack/{id}")
-    public ResponseEntity<Flux<Map>> removeFromStackByIndex(@PathVariable Integer id) {
-        ResponseEntity<Map> response =  restTemplate.exchange("http://ARRAY-LIST/stack/"+id, HttpMethod.DELETE,null, Map.class);
-        return new ResponseEntity(Flux.just(response), response.getStatusCode());
-    }
-
-     .....
-
-
-    */
-
 
 }
